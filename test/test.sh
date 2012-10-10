84ecf7690c1263b62bf2db64a30ef9605a571578
Jari Aalto <jari.aalto@cante.net>
2012-10-10 06:34:51 +0300

    test/test.sh: Use TMPBASE

--- test/test.sh
+++ test/test.sh
@@ -17,7 +17,7 @@ esac
 
 AtExit ()
 {
-    rm -rf "$TMPBASE"
+    rm -rf "$TMPBASE.dir" "$TMPBASE"*
 }
 
 Run ()
@@ -31,20 +31,23 @@ trap AtExit 0 1 2 3 15
 
 # #######################################################################
 
-archive=archive.run
+archive="$TMPBASE-archive.run"
 
-mkdir -p "$TMPBASE"
-( cd "$TMPBASE" && touch start.sh 2 3 4 5 ; chmod 755 start.sh )
+mkdir -p "$TMPBASE.dir"
+( cd "$TMPBASE" && touch example.sh 1 2 3 ; chmod 755 start.sh )
 
-Run "%% TEST create:" makeself --nowait --nomd5 --nocrc --nox11 --nocomp \
-    "$TMPBASE" $archive label echo
+Run "%% TEST create:" makeself \
+ --nowait \
+ --nomd5 \
+ --nocrc \
+ --nox11 \
+ --nocomp \
+ "$TMPBASE" "$archive" label echo
 
 pwd
-ls -la $archive
-file $archive
+ls -la "$archive"
+file "$archive"
 
-Run "%% TEST run:" ./$archive
-
-rm -f $archive
+Run "%% TEST run:" "$archive"
 
 # End of file
